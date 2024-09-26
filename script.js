3eea2963a17ba865812946511e45c07caadbfe48
pransu-coder <pransu145@gmail.com>
2024-09-26 21:31:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-let result = document.getElementById('result');
-let expression = "";
+const populate = async (value, currency) => {
+    let myStr = ""
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_7UStkUqQNBmahSoy8K635tE3Sjr5fK1UVPmVloZ2&base_currency=" + currency
+    let response = await fetch(url)
+    let rJson = await response.json()
+    document.querySelector(".output").style.display = "block"
 
-function appendToResult(value) {
-    expression += value;
-    result.value = expression;
-}
-
-function calculate(operator) {
-    if (operator === '=') {
-        try {
-            result.value = eval(expression);
-            expression = "";
-        } catch (error) {
-            result.value = "Error";
-        }
-    } else {
-        expression += operator;
-        result.value = expression;
+    for (let key of Object.keys(rJson["data"])) {
+        myStr += ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr> 
+                `
     }
-}
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr;
 
-function clearResult() {
-    expression = "";
-    result.value = "";
 }
-
-function deleteLastChar() {
-    expression = expression.slice(0, -1);
-    result.value = expression;
-}
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value
+    populate(value, currency)
+})
